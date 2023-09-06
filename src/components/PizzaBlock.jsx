c53488c563e14fb70fe5c252db5412091253cc35
admin <admin@uladzislaukolbik.local>
2023-09-06 19:53:51 +0300

    feat: add sort selector with toggle

--- src/components/PizzaBlock.jsx
+++ src/components/PizzaBlock.jsx
@@ -12,6 +12,7 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
         <ul>
           {types.map((typeId) => (
             <li
+              key={typeId}
               onClick={() => setActiveType(typeId)}
               className={activeType === typeId ? 'active' : ''}>
               {typeNames[typeId]}
@@ -20,7 +21,10 @@ function PizzaBlock({ title, price, imageUrl, sizes, types }) {
         </ul>
         <ul>
           {sizes.map((size, i) => (
-            <li onClick={() => setActiveSize(i)} className={activeSize === i ? 'active' : ''}>
+            <li
+              key={i}
+              onClick={() => setActiveSize(i)}
+              className={activeSize === i ? 'active' : ''}>
               {size} см.
             </li>
           ))}
